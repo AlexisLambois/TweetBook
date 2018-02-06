2d16de49a6f22791ba8951a829973e629950cf2e
AlexisLambois <lambois16@gmail.com>
2018-02-06 13:26:36 +0100

    06/02 13:26

---
+++ apache-tomcat-8.0.46/webapps/TweetBook/src/tools/Accueil.java
@@ -0,0 +1,33 @@
+package tools;
+
+import java.io.IOException;
+import java.sql.*;
+import javax.naming.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class Accueil
+ **/
+
+@WebServlet("/accueil")
+public class Accueil extends HttpServlet {
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		try{
+			
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			DataSource ds = (DataSource) envCtx.lookup("mabase");
+			Connection con = ds.getConnection();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
